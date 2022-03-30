5c608570f0125ce9f5b24c81bdff3696d2434cfc
rifdahr <rasyadirifdah@gmail.com>
2022-03-30 00:20:57 -0700

    lanjutan studi kasus 2

--- Studi Kasus 2/Struk RM.cpp
+++ Studi Kasus 2/Struk RM.cpp
@@ -49,3 +49,52 @@ harga=17000;
 total=harga*porsi;
 cout<<" Total Harga  = Rp"<<total <<endl;
 }
+else
+if (kode==3)
+{
+cout<<" Nama Makanan = Udang goreng" <<endl;
+cout<<" Jumlah Porsi = ";
+cin>>porsi;
+harga=19000;
+total=harga*porsi;
+cout<<" Total Harga  = Rp"<<total <<endl;
+}
+else
+if (kode==4)
+{
+cout<<" Nama Makanan = Cumi goreng" <<endl;
+cout<<" Jumlah Porsi = ";
+cin>>porsi;
+harga=20000;
+total=harga*porsi;
+cout<<" Total Harga  = Rp"<<total <<endl;
+}
+else
+if (kode==5)
+{
+cout<<" Nama Makanan = Ayam bakar" <<endl;
+cout<<" Jumlah Porsi = ";
+cin>>porsi;
+harga=25000;
+total=harga*porsi;
+cout<<" Total Harga  = Rp"<<total <<endl;
+}
+
+else
+{
+cout<<"-Invalid Number-a";
+goto ulang;
+}
+bayar=bayar+total;
+}
+cout<<endl;
+cout<<endl;
+cout<<"Total Bayar          = Rp"<<bayar<<",-" <<endl;
+cout<<"Masukan Pembayaran   = Rp";cin>>duit;
+cout<<endl;
+cout<<"masukkan jarak ruah anda dari rumah makan = ";//input jarak
+cin>>jarak;
+cout<<"jarak dari rumah ke rumah akan adalah "<<jarak<<"km"<<endl;
+if(jarak<=3){
+if(bayar>=25000){
+ongkir = 3000;
